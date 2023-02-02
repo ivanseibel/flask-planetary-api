b915f9801a9c935ab236bceb24a8fbc2ecff90a5
Ivan Seibel <me@ivanseibel.dev>
2023-02-02 17:07:22 +0000

    initial app - hello world route - working with parameters - returning json

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+  # Gets user name that comes from the request
+  name = request.args.get('name')
+  
+  # If user name is not provided, return a message
+  if name is None:
+    return jsonify({'message': 'Please provide a name'})
+
+  # If user name is provided, return a message
+  return jsonify({'message': f'Hello {name}'})
+
+
+@app.route('/parameters')
+def parameters():
+  age = int(request.args.get('age'))
+  name = request.args.get('name')
+
+  if age < 18:
+    return ({
+      'message': f'Sorry {name}, you are not old enough to view this page'
+    }), 401
+
+  return ({
+    'message': f'Welcome {name}, you are old enough to view this page'
+  })
+
+
+if __name__ == '__main__':
+  app.run()
