98ac467dcacb2979bc8ca13545422879526fee0f
Ivan Seibel <me@ivanseibel.dev>
2023-02-03 16:27:07 +0000

    creates and set up marshmallow instance

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 import os
+from flask_marshmallow import Marshmallow
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'planets.db')
+
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 
 @app.cli.command('db_create')
