dd2ad79e3e1225ed085de1646b0360fbfbbedb1f
Ivan Seibel <me@ivanseibel.dev>
2023-02-03 16:26:40 +0000

    serializes db data with marshmallow

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float
-from app import db
+from app import db, ma
+
 
 class User(db.Model):
   __tablename__ = 'users'
@@ -19,3 +20,19 @@ class Planet(db.Model):
   mass = Column(Float)
   radius =  Column(Float)
   distance = Column(Float)
+
+
+class UserSchema(ma.Schema):
+  class Meta:
+    fields = ('id', 'first_name', 'last_name', 'email', 'password')
+
+class PlanetSchema(ma.Schema):
+  class Meta:
+    fields = ('planet_id', 'planet_name', 'planet_type', 'home_star', 'mass', 'radius', 'distance')
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+planet_schema = PlanetSchema()
+planets_schema = PlanetSchema(many=True)
